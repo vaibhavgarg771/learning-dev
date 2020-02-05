21643ac48dda47eaed395c0050ee82b84cbef316
Vaibhav Garg <vaibhav.garg771@gmail.com>
2020-02-05 23:58:43 +0530

    Bike view registration

--- bike/bikes-ui/src/app/app-routing.module.ts
+++ bike/bikes-ui/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 const routes: Routes = [
 
-  {path:'admin',  component: AdminComponent}, 
-  {path:'home', component: HomeComponent},
+  { path: 'admin/view/:id', component: ViewRegistrationComponent},
+  { path: 'admin',  component: AdminComponent}, 
+  { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
--- bike/bikes-ui/src/app/components/view-registration/view-registration.component.ts
+++ bike/bikes-ui/src/app/components/view-registration/view-registration.component.ts
@@ -1,12 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { BikeService } from 'src/app/services/bike.service';
+import { IBike } from 'src/app/services/bike.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
+    selector:'app-view-registraion',
     templateUrl:'view-registration.component.html',
 })
 
-export class ViewRegistrationComponent{
-    constructor(private bikeService: BikeService){
+export class ViewRegistrationComponent implements OnInit{
+    
+    public bikeReg: IBike;
+    constructor(private bikeService: BikeService, private route: ActivatedRoute){
 
     }
+
+    ngOnInit(){
+        this.getBikeReg(this.route.snapshot.params.id);
+    }
+
+    getBikeReg(id:number){
+        this.bikeService.getBike(id).subscribe(
+            bike => {
+                this.bikeReg = <IBike>bike;
+                this.bikeReg.contact = true;
+            }, 
+            err => console.log(err), 
+            () => console.log("bikes Loaded")
+            );
+    }
+
 }
